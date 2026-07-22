a6d616bd5b354dd834d6ada53ce7dd76947b46f8
Marcelo Miranda <mmiranda@mmiranda.local>
2026-07-22 13:29:51 -0300

    chore: Flutter 3.44.7 upgrade, iOS simulator build support
    - Upgrade Flutter SDK from 3.32.8 to 3.44.7
    - Add Podfile post_install hook to clear extended attributes before codesigning
      (fixes 'resource fork, Finder information, or similar detritus not allowed'
      errors when building for iOS Simulator)
    - Disable Swift Package Manager for Flutter (plugin compatibility)
    - Update generated platform files for new Flutter version

--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import flutter_secure_storage_macos
 import local_auth_darwin
 import mobile_scanner
 import path_provider_foundation
+import share_plus
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
